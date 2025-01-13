5c4bfae16e5bfc0d7f9558d7cd086d3669fbf67e
Ekur98 <esa.kurtti@hotmail.com>
2025-01-13 23:30:22 +0200

    code changes

---
+++ custom_assets/jak1/levels/sandpit/sandpit.gd
@@ -0,0 +1,25 @@
+;; DGO definition file for Sandpit level
+;; We use the convention of having a longer DGO name for levels without precomputed visibility.
+
+;; the actual file name still needs to be 8.3
+("SNP.DGO"
+ ("test-zone-obs.o"
+  "linear-plat.o"
+  "tpage-398.go"
+  "tpage-400.go"
+  "tpage-399.go"
+  "tpage-401.go"
+  "tpage-1470.go"
+  "plat-ag.go"
+  "plat-eco-ag.go"
+  "orb-cache-top-ag.go"
+  "ecovalve-ag.go"
+  "test-actor-ag.go"
+  "babak-ag.go"
+  "lurkerworm.o"
+  "lurkerworm-ag.go"
+  "eichar-pole+0-ag.go"
+  "swamp-rock-ag.go"
+  "sandpit.go"
+  )
+ )
--- goal_src/jak1/game.gp
+++ goal_src/jak1/game.gp
@@ -1659,6 +1659,13 @@
 ;; the DGO file
 (custom-level-cgo "TSZ.DGO" "test-zone/testzone.gd")
 
+;; sandpit level
+
+;; it should point to the .jsonc file that specifies the level.
+(build-custom-level "sandpit")
+;; the DGO file
+(custom-level-cgo "SNP.DGO" "sandpit/sandpit.gd")
+
 ;; generate the art group for a custom actor.
 ;; requires a .glb model file in custom_assets/jak1/models/custom_levels
 ;; to also generate a collide-mesh, add :gen-mesh #t
